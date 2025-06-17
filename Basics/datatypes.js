d9200b87d06c8ffef16bd22170dc66afd774394b
Nitish <71253349+nitishdesigner@users.noreply.github.com>
2025-06-17 09:35:23 +0000

    datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treal all js code as new version
+
+//alert(3+3)// we are using nodejs, not browser
+
+let name = "nitish"
+let age = 20
+let isLoggedIn = false
+let state;
+
+//number is 2 to power 53
+//bigint    for big numbers e.g for trading
+//string use in "" or '' 
+// boolean = true/false i.e only two values, 
+//null standlone value i.e is a special type, empty
+//undefined i.e value not assisned
+//symbol i.e unquieness
+
+//object
+
+console.log(typeof null); // object 
+console.log(typeof undefined); // undefined
---
+++ Basics/variables.js
@@ -0,0 +1,9 @@
+const accId= 13344;
+let accEmail ="ladda@gmail.com"
+var accpass ="927"
+accState ="mohali"
+
+let accName; /* variable can be declare, value not assigned but this can reverse the space or memory */
+console.log(accId);
+console.table([accId, accEmail, accpass, accState,accName]);
+
