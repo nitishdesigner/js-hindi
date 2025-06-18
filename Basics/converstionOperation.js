31fe557aed46666c75ccc7756ccdcd85ea32bf25
Nitish <71253349+nitishdesigner@users.noreply.github.com>
2025-06-18 07:12:56 +0000

    consverstion of type

---
+++ Basics/converstionOperation.js
@@ -0,0 +1,17 @@
+let score = 3
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+//make it string
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
